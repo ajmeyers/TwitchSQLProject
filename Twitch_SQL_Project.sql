580d4a70da4b3f851dce52b1389db252be502048
Andrew J. Meyers <ameyers.central@gmail.com>
2018-07-21 09:11:00 -0400

    Twitch SQL Project - Solutions

--- Twitch_SQL_Project.sql
+++ Twitch_SQL_Project.sql
@@ -4,21 +4,21 @@
 */
 
 
-Question 1) 
+/* Question 1 */
  
 SELECT * FROM stream LIMIT 20;
 
  /* Free Answer */
 
 
-Question 2) 
+/* Question 2 */ 
 
 SELECT * FROM chat LIMIT 20; 
 
 /* Gets first 20 rows from chat */
 
 
-Question 3)
+/* Question 3 */ 
 
 DELETE FROM chat 
 WHERE time = 'time'; 
@@ -26,7 +26,7 @@ WHERE time = 'time';
 /* Removes first row from chat */
 
 
-Question 4) 
+/* Question 4 */ 
 
 SELECT DISTINCT game 
 FROM stream 
@@ -35,7 +35,7 @@ WHERE game IS NOT NULL;
 /* Gets all Distinct Games from stream table, some games are NULL */
 
 
-Question 5)
+/* Question 5 */ 
 
 SELECT DISTINCT channel 
 FROM stream;
@@ -43,7 +43,7 @@ FROM stream;
 /* Gets all Distinct Channels from stream table */
 
 
-Question 6)
+/* Question 6 */ 
 
 SELECT game AS games, COUNT(*) AS viewers 
 FROM stream 
@@ -53,7 +53,7 @@ ORDER BY viewers DESC;
 /* 15 viewers view a Null game */
 
 
-Question 7)
+/* Question 7 */ 
 
 SELECT country AS Location, COUNT(*) AS Number_of_League_Players 
 FROM stream 
@@ -64,14 +64,14 @@ ORDER BY Number_of_League_Players DESC;
 /* 49 League Players come from a NULL Location */
 
 
-Question 8)
+/* Question 8 */ 
 
 SELECT player AS Players, COUNT(*) AS Streamers FROM stream GROUP BY Players ORDER BY Streamers DESC; 
 
 /* Most Streamers watch games on site */
 
 
-Question 9)
+/* Question 9 */ 
 
 SELECT game,
 CASE 
@@ -87,10 +87,14 @@ ORDER by genre;
 /* games with NULL value receive genre as 'Others' */
 
 
-Question 10)
+/* Question 10 */ 
 
 SELECT chat.device_id 
 FROM chat 
 INNER JOIN stream ON stream.device_id=chat.device_id;
 
 /* INNER JOIN used on both device_id columns */
+
+
+/* Bonus */ 
+
